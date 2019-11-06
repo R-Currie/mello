021b7a09d4eabc8ff9f07bf2e43b0ef057bd664e
R-Currie <currierobertm@gmail.com>
2019-11-05 21:35:04 -0600

    Final front end

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,12 +1,16 @@
 class Card {
     constructor(text){
         this.text = text;
+        this.id = ++Card.lastId;
     }
 }
+
+Card.lastId = 0;
 
 class List {
     constructor(title){
         this.title = title;
+        this.id = ++List.lastId;
         this.cards = [];
     }
 
@@ -14,21 +18,57 @@ class List {
         let card = new Card(text);
         this.cards.push(card);
     }
+
+    findCard(cardId) {
+        return this.cards.find(function(card) {
+          if (cardId === card.id) {
+            return card;
+          }
+        });
+      }
 }
+
+List.lastId = 0;
 
 class Board {
-    constructor(){
-        this.lists = [];
+    constructor() {
+      this.id = ++Board.lastId;
+      this.lists = [];
     }
+  
+    addList(text) {
+      var list = new List(text);
+      this.lists.push(list);
+    }
+
+    findList(listId) {
+        return this.lists.find(function(list) {
+          return listId === list.id;
+        });
+      }
     
-    addList = text => {
-        let list = new List(text);
-        this.lists.push(list);
-    }
-}
+      editList(listId, newTitle) {
+        var list = this.findList(listId);
+        if (list) {
+          list.title = newTitle;
+        }
+      }
+    
+      addCard(listId, cardText) {
+        var list = this.findList(listId);
+        if (list) {
+          list.addCard(cardText);
+        }
+      }
 
-var board = new Board();
-board.addList('New List');
-board.lists[0].addCard('New Card');
+      editCard(cardId, cardText) {
+        this.lists.forEach(function(list) {
+          var card = list.findCard(cardId);
+          if (card) {
+            card.text = cardText;
+          }
+        });
+      }
+  }
 
-console.log(board);
+Board.lastId = 0;
